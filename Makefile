5b50969c276cd5ca0ffc4e7e8b1366b412e44ea3
Jonathan Klein <joklein@1-B-14.42heilbronn.de>
2024-12-12 13:39:21 +0100

    overwork Makefile, so ft_printf useable

--- Makefile
+++ Makefile
@@ -3,19 +3,24 @@ MLX42_URL	= https://github.com/codam-coding-college/MLX42.git
 MLX42_DIR	= MLX42
 BUILD_DIR	= $(MLX42_DIR)/build
 
-SRC_DIR		= src
+LIBFT_DIR   = libft
+PRINTF_DIR  = $(LIBFT_DIR)/printf
 
+SRC_DIR		= src
 SRCS		= $(SRC_DIR)/so_long.c
-OBJS		= ${SRCS:.c=.o}
+
+OBJS = ${SRCS:.c=.o} ${PRINTF_DIR}/libftprintf.a
 
 NAME		= so_long
+HEADER 		= $(SRC_DIR)/so_long.h
 CC			= gcc
 CFLAGS		= -Wextra -Wall -Werror
 LDFLAGS		= -ldl -lglfw -lm -lpthread -L$(BUILD_DIR) -lmlx42 -I$(MLX42_DIR)
 
+all: printf $(NAME)
 
-all: $(NAME)
-
+printf:
+	@make -C ${PRINTF_DIR}
 
 $(MLX42_DIR):
 	@printf "Cloning MLX42... "
@@ -30,7 +35,7 @@ $(BUILD_DIR): $(MLX42_DIR)
 	@cd $(MLX42_DIR) && cmake --build build -j4 > /dev/null
 	@printf "\033[0;32mDone.\033[0m\n"
 
-$(OBJS): $(SRCS)
+$(SRC_DIR)/%.o: $(SRC_DIR)/%.c
 	@printf "Compiling $<... "
 	@$(CC) $(CFLAGS) -I$(MLX42_DIR) -c $< -o $@ > /dev/null
 	@printf "\033[0;32mDone.\033[0m\n"
@@ -41,15 +46,16 @@ $(NAME): $(BUILD_DIR) $(OBJS)
 	@printf "\033[0;32mDone.\033[0m\n"
 	@printf "\033[0;32mso_long successfully built!\033[0m\n"
 
-
 clean:
-	rm -rf $(BUILD_DIR) $(NAME)
-	rm -f $(OBJS)
+	rm -rf $(BUILD_DIR)
+	rm -f ${SRCS:.c=.o}
+	@make -C ${PRINTF_DIR} -f Makefile clean
 
 fclean: clean
+	rm -f so_long
+	@make -C ${PRINTF_DIR} -f Makefile fclean
 	rm -rf $(MLX42_DIR)
-	rm -f $(NAME)
 
 re: fclean all
 
-.PHONY: all src clean fclean re
+.PHONY: all printf src clean fclean re
--- src/so_long.c
+++ src/so_long.c
@@ -6,7 +6,7 @@
 /*   By: joklein <joklein@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/09 15:13:19 by joklein           #+#    #+#             */
-/*   Updated: 2024/12/11 17:03:39 by joklein          ###   ########.fr       */
+/*   Updated: 2024/12/12 12:40:54 by joklein          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,23 +15,40 @@
 
 void	ft_hook(void *param)
 {
-	t_data	*data;
+	t_data		*data;
+	static int	moves = 0;
 
 	data = param;
 	if (mlx_is_key_down(data->mlx, MLX_KEY_ESCAPE))
 		mlx_close_window(data->mlx);
-	if ((mlx_is_key_down(data->mlx, MLX_KEY_UP) || mlx_is_key_down(data->mlx, MLX_KEY_W))
-		&& data->image->instances[0].y > 30)
+	if ((mlx_is_key_down(data->mlx, MLX_KEY_UP) || mlx_is_key_down(data->mlx,
+				MLX_KEY_W)) && data->image->instances[0].y > 30)
+	{
 		data->image->instances[0].y -= 30;
-	if ((mlx_is_key_down(data->mlx, MLX_KEY_DOWN) || mlx_is_key_down(data->mlx, MLX_KEY_S))
-		&& data->image->instances[0].y < data->height - 60)
+		moves++;
+		printf("%i", moves);
+	}
+	if ((mlx_is_key_down(data->mlx, MLX_KEY_DOWN) || mlx_is_key_down(data->mlx,
+				MLX_KEY_S)) && data->image->instances[0].y < data->height - 60)
+	{
 		data->image->instances[0].y += 30;
-	if ((mlx_is_key_down(data->mlx, MLX_KEY_LEFT) || mlx_is_key_down(data->mlx, MLX_KEY_A))
-		&& data->image->instances[0].x > 30)
+		moves++;
+		printf("%i", moves);
+	}
+	if ((mlx_is_key_down(data->mlx, MLX_KEY_LEFT) || mlx_is_key_down(data->mlx,
+				MLX_KEY_A)) && data->image->instances[0].x > 30)
+	{
 		data->image->instances[0].x -= 30;
-	if ((mlx_is_key_down(data->mlx, MLX_KEY_RIGHT) || mlx_is_key_down(data->mlx, MLX_KEY_D))
-		&& data->image->instances[0].x < data->width - 60)
+		moves++;
+		printf("%i", moves);
+	}
+	if ((mlx_is_key_down(data->mlx, MLX_KEY_RIGHT) || mlx_is_key_down(data->mlx,
+				MLX_KEY_D)) && data->image->instances[0].x < data->width - 60)
+	{
 		data->image->instances[0].x += 30;
+		moves++;
+		printf("%i", moves);
+	}
 	usleep(100000);
 }
 
@@ -65,7 +82,7 @@ int	set_ground(mlx_t *mlx_window, t_data *data)
 	return (mlx_delete_texture(ground_texture), 0);
 }
 
-void c()
+void	c(void)
 {
 	system("leaks so_long");
 }
@@ -77,40 +94,34 @@ int	main(void)
 	int				height;
 	int				width;
 
-	//atexit(c);
+	// atexit(c);
 	width = 450;
 	height = 300;
 	data = malloc(sizeof(t_data));
-	
 	data->width = width;
 	data->height = height;
-	
-
 	data->mlx = mlx_init(data->width, data->height, "MLX Window", true);
 	if (!data->mlx)
 		return (free(data), write(1, "Error\n", 6), 1);
-	
-
-	
 	if (set_ground(data->mlx, data) == 1)
-		return (free(data),write(1, "Error\n", 6), 1);
+		return (free(data), write(1, "Error\n", 6), 1);
 	thief_texture = mlx_load_png("images/Thief.png");
 	if (!thief_texture)
 	{
 		mlx_close_window(data->mlx);
-		return (free(data),write(1, "Error\n", 6), 1);
+		return (free(data), write(1, "Error\n", 6), 1);
 	}
 	data->image = mlx_texture_to_image(data->mlx, thief_texture);
 	if (!data->image)
 	{
 		mlx_close_window(data->mlx);
 		mlx_delete_texture(thief_texture);
-		return (free(data),write(1, "Error\n", 6), 1);
+		return (free(data), write(1, "Error\n", 6), 1);
 	}
 	if (mlx_image_to_window(data->mlx, data->image, 30, 30) == -1)
 	{
 		mlx_close_window(data->mlx);
-		return (free(data),write(1, "Error\n", 6), 1);
+		return (free(data), write(1, "Error\n", 6), 1);
 	}
 	mlx_delete_texture(thief_texture);
 	mlx_loop_hook(data->mlx, ft_hook, data);
@@ -120,4 +131,4 @@ int	main(void)
 	return (0);
 }
 
-//system("leaks so_long");
+// system("leaks so_long");
